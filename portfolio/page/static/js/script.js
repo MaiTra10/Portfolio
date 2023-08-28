a08c8b4f268fc79da6c029af274eef48d772e3ec
Maitra Patel <10maitra22@gmail.com>
2023-08-28 09:43:40 -0600

    feat
    added the base html, css and js

---
+++ portfolio/page/static/js/script.js
@@ -0,0 +1,9 @@
+let welcomeBlock = document.getElementsByClassName('div_welcome')[0];
+
+console.log(welcomeBlock)
+
+function goToAPI() {
+
+    welcomeBlock.classList.add('div_welcome-hide');
+
+}
