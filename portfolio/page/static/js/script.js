c96e6e96b7d43d2b7a269427aa9f907b11a6f9c8
Maitra Patel <10maitra22@gmail.com>
2023-08-31 18:17:04 -0600

    feat
    added the API endpoint input and transition to the API use part

--- portfolio/page/static/js/script.js
+++ portfolio/page/static/js/script.js
@@ -1,9 +1,17 @@
 let welcomeBlock = document.getElementsByClassName('div_welcome')[0];
+let container = document.getElementsByClassName('div_container')[0];
 
 console.log(welcomeBlock)
 
 function goToAPI() {
 
     welcomeBlock.classList.add('div_welcome-hide');
+        
+    setTimeout(function() {
+
+        welcomeBlock.remove();
+        container.classList.add('div_container-show');
+
+    }, 250)
 
 }
